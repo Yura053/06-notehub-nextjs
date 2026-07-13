6d3d980df9dd9a6c87192977ef4e4980ead58e6e
Yurii Ivanes <ivanecura82@gmail.com>
2026-07-13 17:41:35 +0300

    fix10

---
+++ components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import css from "./ErrorMessage.module.css";
+
+export default function ErrorMessage() {
+  return (
+    <>
+      <p className={css.error_text}>
+        <hr />
+        There was an error, please try again...
+      </p>
+    </>
+  );
+}
