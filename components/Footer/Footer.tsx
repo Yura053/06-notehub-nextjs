09bebb5674039795771a4d2503dda58bfe7c22e5
Yurii Ivanets <161445113+Yura053@users.noreply.github.com>
2026-07-19 01:31:19 +0300

    Update developer information in footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,10 +6,10 @@ export default function Footer() {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: Drochak Stanislav</p>
+          <p>Developer: Ivanets Yurii</p>
           <p>
             Contact us:
-            <a href="mailto:stasdroner@gmail.com"> stasdroner@gmail.com</a>
+            <a href="mailto:ivanecura82@gmail.com"> ivanecura82@gmail.com</a>
           </p>
         </div>
       </div>
